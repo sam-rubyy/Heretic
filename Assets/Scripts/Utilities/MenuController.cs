945b8cbdba5595670d4335f2333337cadd8f8a04
Leo <zexiyin2003@gmail.com>
2025-12-04 01:05:54 -0500

    Add sound effect, but have some bugs

--- Assets/Scripts/Utilities/MenuController.cs
+++ Assets/Scripts/Utilities/MenuController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -32,5 +43,36 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
